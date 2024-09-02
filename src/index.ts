75d66a4ee3de6648c9a0897d6dcacf51895c09aa
Александр Данилюк <andanilyuk@mts.ru>
2024-09-03 01:14:18 +0300

    shared module

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,21 @@
-import { Bot, Context, } from "grammy";
 import EventEmitter from "node:events";
+import { TelegramBot } from "./telegram-bot.class";
+const packageInfo = require('../package.json');
 
-export interface SetUrlEvent {
-  type: 'set_url';
-  url: string;
-}
+export class Module {
+  private bot: TelegramBot;
 
-export interface BotStartEvent {
-  type: 'bot-start';
-}
-
-export type DomainEvent = BotStartEvent | SetUrlEvent;
-
-export type DomainEventType = DomainEvent['type'];
-
-export type DomainEventData<T extends DomainEventType = DomainEventType> = Extract<
-  DomainEvent,
-  {
-    type: T;
-  }
->;
-
-export type DomainEventPayload<T extends DomainEventType = DomainEventType> = Omit<DomainEventData<T>, 'type'>;
-//  announce<T extends DomainEvent>(event: T, context?: DomainEventsContext<T['type']>): void {
-export class TelegramBot {
-  protected bot: Bot<Context>;
-  protected eventBus: EventEmitter;
-
-  constructor(config: {telegramBotToken: string}, eventBus: EventEmitter){
-    this.bot = new Bot(config.telegramBotToken);
-    this.eventBus = eventBus;
+  async onInit(config: {telegramBotToken: string}, eventBus: EventEmitter): Promise<void> {
+    this.bot = new TelegramBot(config, eventBus);
   }
 
-  public async sendMessage(chat_id: number, msg: string): Promise<void> {
-    await this.bot.api.sendMessage(chat_id, msg);
+  async Run(): Promise<void> {
+    this.bot.startBot()
   }
 
-  public async startBot(): Promise<void> {
-
-
-    //this.bot.on("message", (ctx) => ctx.reply("Hi there!"));
-    this.bot.command("url", async (ctx) => {
-      // `item` will be "apple pie" if a user sends "/add apple pie".
-      try {
-      const url = new URL(ctx.match);
-      this.eventBus.emit('set_url', url);
-      return ctx.reply("I take the URL for processing");
-    } catch (error) {
-      this.eventBus.emit('invalid_url');
-      return ctx.reply("The URL invalid");
-    }
-      
-      
-    });
-
-    this.bot.start();
+  async Info(): Promise<{name: string, version: string}> {
+    return Promise.resolve({name: packageInfo.name, version: packageInfo.version}); 
   }
+
+  async onDestroy(): Promise<void>  {}
 }
---
+++ src/telegram-bot.class.ts
@@ -0,0 +1,63 @@
+import { Bot, Context, } from "grammy";
+import EventEmitter from "node:events";
+
+export interface SetUrlEvent {
+  type: 'set_url';
+  url: string;
+}
+
+export interface BotStartEvent {
+  type: 'bot-start';
+}
+
+export type DomainEvent = BotStartEvent | SetUrlEvent;
+
+export type DomainEventType = DomainEvent['type'];
+
+export type DomainEventData<T extends DomainEventType = DomainEventType> = Extract<
+  DomainEvent,
+  {
+    type: T;
+  }
+>;
+
+export type DomainEventPayload<T extends DomainEventType = DomainEventType> = Omit<DomainEventData<T>, 'type'>;
+//  announce<T extends DomainEvent>(event: T, context?: DomainEventsContext<T['type']>): void {
+export class TelegramBot {
+  protected bot: Bot<Context>;
+  protected eventBus: EventEmitter;
+
+  constructor(config: {telegramBotToken: string}, eventBus: EventEmitter){
+    this.bot = new Bot(config.telegramBotToken);
+    this.eventBus = eventBus;
+  }
+
+  public async sendMessage(chat_id: number, msg: string): Promise<void> {
+    await this.bot.api.sendMessage(chat_id, msg);
+  }
+
+  public async startBot(): Promise<void> {
+
+
+    //this.bot.on("message", (ctx) => ctx.reply("Hi there!"));
+    this.bot.command("url", async (ctx) => {
+      // `item` will be "apple pie" if a user sends "/add apple pie".
+      try {
+      const url = new URL(ctx.match);
+      this.eventBus.emit('set_url', url.href);
+      return ctx.reply("I take the URL for processing");
+    } catch (error) {
+      this.eventBus.emit('invalid_url');
+      return ctx.reply("The URL invalid");
+    }
+      
+      
+    });
+
+    this.bot.start();
+  }
+
+  public async stopBot(): Promise<void> {
+    this.bot.stop();
+  }
+}
